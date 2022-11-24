61ad0f9c199ae35704111d3e036a37ede1913f80
LutherMPG <mgrosenp@outlook.dk>
2022-11-24 16:00:09 +0100

    Let´´ rock and roll!

--- Testproject/src/Main.java
+++ Testproject/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Så er der kage!");
         System.out.println("Jaaaeeerr!!");
         System.out.println("Men er der kaffe?....");
-        System.out.println("Tee anybody?....");
+        System.out.println("Tee anybody?...");
     }
 
 }
