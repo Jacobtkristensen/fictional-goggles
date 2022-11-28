371f450dff2b95b33483d9c0af11e6e25fe11577
Lotte Eliasson <lottefacereliasson@hotmail.com>
2022-11-28 11:47:04 +0100

    Revert "Let´´ rock and roll!"
    This reverts commit 61ad0f9c

--- Testproject/src/Delfin.java
+++ Testproject/src/Delfin.java
@@ -1,11 +1,9 @@
-public class Delfin {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Så er der kage!");
         System.out.println("Jaaaeeerr!!");
         System.out.println("Men er der kaffe?....");
-        System.out.println("Tee anybody?...");
-        System.out.println("måske virker dette her.....");
-        System.out.println("Virker min Branch måske også??????");
+        System.out.println("Tee anybody?....");
     }
 
 }
