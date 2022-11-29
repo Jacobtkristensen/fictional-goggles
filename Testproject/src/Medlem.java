311cfbf224df2c3bb1d1056e533b67a84facd4d8
Gi5lund <mortengislund@gmail.com>
2022-11-29 09:07:50 +0100

    date ændret til localdatetime

--- Testproject/src/Medlem.java
+++ Testproject/src/Medlem.java
@@ -1,17 +1,20 @@
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public abstract class Medlem implements Kontingent {
-
+	private static int medlemmer
 	private int medlemsnummer;
 	private String navn;
-	private Date fødseldag;
+	private LocalDateTime fødseldag;
 	private boolean gender;
+
+
 
 	/**
 	 * 
 	 * @param f�dselsdag
 	 */
-	public int getAlder(Date fødselsdag) {
+	public int getAlder(LocalDateTime fødselsdag) {
 		// TODO - implement Medlem.getAlder
 		throw new UnsupportedOperationException();
 	}
