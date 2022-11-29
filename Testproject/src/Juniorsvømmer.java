2bc09bd093f33300d6ad539248f805352fd13324
Gi5lund <mortengislund@gmail.com>
2022-11-29 12:09:35 +0100

    rettet opretmdlem

--- Testproject/src/Juniorsvømmer.java
+++ Testproject/src/Juniorsvømmer.java
@@ -1,5 +1,29 @@
+import java.time.LocalDate;
+
 public class Juniorsvømmer extends Konkurrencesvømmer {
 
 	private Svømmedisciplin[] discipliner;
+	private boolean[] aktivdisciplin=boolean[4];
+	//Brystsvømning, Crawl, Rygsvømning, Butterfly
+
+	public Juniorsvømmer(String navn, LocalDate fødseldag, boolean gender, String aktivdisciplin){
+		super(navn,fødseldag,gender);
+		this.aktivdisciplin=setAktivDiscipliner(aktivdisciplin);
+	}
+
+	public void setAktivDiscipliner(String disciplinset) {
+		if(disciplinset.contains("b")){
+			aktivdisciplin[0]=true;
+		}
+		if(disciplinset.contains("c")){
+			aktivdisciplin[1]=true;
+		}
+		if(disciplinset.contains("r")){
+			aktivdisciplin[2]=true;
+		}
+		if(disciplinset.contains("f")){
+			aktivdisciplin[3]=true;
+		}
+	}
 
 }
