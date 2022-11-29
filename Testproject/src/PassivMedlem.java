4603ff6d35de7c95ed2661591eae739936595ec1
Gi5lund <mortengislund@gmail.com>
2022-11-29 11:37:08 +0100

    PassivMedlem

--- Testproject/src/PassivMedlem.java
+++ Testproject/src/PassivMedlem.java
@@ -1,2 +1,7 @@
+import java.time.LocalDate;
+
 public class PassivMedlem extends Medlem {
+    public PassivMedlem(String navn, LocalDate fødseldag, boolean gender){
+        super(navn, fødseldag, gender);
+    }
 }
