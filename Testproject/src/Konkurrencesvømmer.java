7266a37edd7422bf0357a6290f4d03960f58cd33
Gi5lund <mortengislund@gmail.com>
2022-11-30 23:18:58 +0100

    safety commit

--- Testproject/src/Konkurrencesvømmer.java
+++ Testproject/src/Konkurrencesvømmer.java
@@ -1,8 +1,11 @@
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Arrays;
 
 public class Konkurrencesvømmer extends Medlem {
 	private boolean[] aktivdisciplin=new boolean[4];
 	private Svømmedisciplin[] discipliner=new Svømmedisciplin[4];
+	private LocalTime[] resultater=new LocalTime[4];
 	public Konkurrencesvømmer(String navn, LocalDate bday, boolean gender, String disciplinset){
 		super(navn,bday,gender);
 		this.aktivdisciplin=setAktivDiscipliner(disciplinset);
@@ -22,6 +25,9 @@ public class Konkurrencesvømmer extends Medlem {
 				}
 			}
 		}
+		String tider="23:59:59,00";
+		LocalTime initialtider=LocalTime.parse(tider);
+		Arrays.fill(resultater,initialtider);
 	}
 
 
@@ -43,6 +49,22 @@ public class Konkurrencesvømmer extends Medlem {
 	}
 	public void GetResults() {
 		System.out.println("to be implemented");
+		for (int i=0;i<4;i++){
+			if(aktivdisciplin[i]){
+				if(i==0){    //"brystsvømning=b, crawl=c, ryg=r, butterfly=f"
+					discipliner[i]=new Svømmedisciplin("brystsvømning");
+				}
+				if(i==1){
+					discipliner[i]=new Svømmedisciplin("crawl");
+				}
+				if(i==2){
+					discipliner[i]=new Svømmedisciplin("ryg");
+				}
+				if(i==3){
+					discipliner[i]=new Svømmedisciplin("butterfly");
+				}
+			}
+		}
 	}
 
 
