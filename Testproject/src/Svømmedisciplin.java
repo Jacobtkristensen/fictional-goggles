efd238646a06088d99f6be0c203db42aeaf6175f
Gi5lund <mortengislund@gmail.com>
2022-12-05 09:16:04 +0100

    fix

--- Testproject/src/Svømmedisciplin.java
+++ Testproject/src/Svømmedisciplin.java
@@ -1,8 +1,23 @@
+import java.io.Serializable;
 import java.time.LocalTime;
 import java.util.Scanner;
 
-public class Svømmedisciplin {
+public class Svømmedisciplin implements Serializable {
+	private String disciplinNavn;
+	private Resultat resultater;
+	private LocalTime currentbest;
 
+	//constructors...noarg constructor must be found
+	public Svømmedisciplin(){
+		this.disciplinNavn="NyDisciplin";
+		this.resultater=new Resultat();
+		this.currentbest=this.resultater.getResult();
+	}
+	public Svømmedisciplin(String disciplinNavn){
+		this.disciplinNavn=disciplinNavn;
+		this.resultater=new Resultat();
+		this.currentbest=this.resultater.getResult();
+	}
 	public String getDisciplinNavn() {
 		return disciplinNavn;
 	}
@@ -10,25 +25,12 @@ public class Svømmedisciplin {
 	public Resultat getResultater() {
 		return resultater;
 	}
-
-	private String disciplinNavn;
-	private Resultat resultater;
-	private LocalTime currentbest;
-
-	public Svømmedisciplin(String disciplinNavn){
-		this.disciplinNavn=disciplinNavn;
-		this.resultater=new Resultat();
-		this.currentbest=this.resultater.getResult();
-	}
 	public String toString(){
 		return "["+ this.getDisciplinNavn()+", "+getResultater().getResult()+"]";
 	}
 	public void opdaterResutater(int trænerinput) { //del af trænerens muligheder
-		// TODO - implement Svømmedisciplin.opdaterResutater
+
 		Scanner sc= new Scanner(System.in);
-		//System.out.println("[1]: træningsresultat \n[2]: stævneresultat ");
-		//System.out.println("indtast valg"); //husk try-catch eller default...
-		//String input=sc.nextInt();
 		switch (trænerinput){
 			case 1:
 			System.out.println("indtast træningsdato som [YYYY-MM-DD]: ");
