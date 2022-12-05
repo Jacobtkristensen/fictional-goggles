06ff6d3c929252ef28a6db6d07a9702a54f8b313
Lotte Eliasson <lottefacereliasson@hotmail.com>
2022-12-05 13:30:12 +0100

    Menu class

--- Testproject/src/Delfinen.java
+++ Testproject/src/Delfinen.java
@@ -85,40 +85,11 @@ public class Delfinen {
 		for(Medlem m:medlemmer){
 			System.out.println(m);
 		}
-		//Konkurrencesvømmer m1=(Konkurrencesvømmer) medlemmer.get(0);
-		//m1.getDiscipliner()[0].opdaterResutater(1);
-		//System.out.println(m1);
-	/*	ArrayList<Medlem> test=new ArrayList<>();
-		try {
-			test= indlæsMedlemmer();
-			System.out.println(test.get(0));
-		}
-		catch (FileNotFoundException f){
-			System.out.println("something went south..sour..sucked.. "+f);
-		}*/
+
+		//Indlæs medlemsliste, hvis tom opret nogle medlemmer.
+		//Menu.hovedmenu();
 
 
-/*
-		System.out.println("Hvad ønsker du at gøre? ");
-		System.out.println("[1] Stamoplysninger.");
-		System.out.println("[2] Kontingentoplysninger.");
-		System.out.println("[3] Svømmeresultater.");
-		System.out.println("[4] Indlæs eller skriv til Medlemsfil.");
-
-		try { //Hovedmenu.
-			Scanner scn = new Scanner(System.in);
-
-			switch (scn.nextInt()) {
-				case 1: Menu.StamoplysningMenu();
-				case 2: Menu.Kontingentmenu();
-				case 3: Menu.SvømmeresultatMenu();
-				case 4: Menu.IndlæsSkrivMedlemsfil();
-			}
-		} catch (Exception e) {
-			System.out.println("Forkert indtastning, prøv igen");
-		}
-
-*/
 		System.out.println("hej");
 
 	}
