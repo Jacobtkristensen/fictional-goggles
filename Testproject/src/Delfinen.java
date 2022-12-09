3ae563230b173fe1ab84a118243a76b953a26217
Jacob <jaco170w@stud.kea.dk>
2022-12-09 09:36:37 +0100

    ryddet op og slettet gamle metoder i Delfinen.java

--- Testproject/src/Delfinen.java
+++ Testproject/src/Delfinen.java
@@ -35,23 +35,6 @@ public class Delfinen {
 		System.out.println("something sucked "+e);
 	}
 	return medlemmer;*/
-
-
-	public void hentkontingent() {
-		// TODO - implement Delfinen.hentkontingent
-		throw new UnsupportedOperationException();
-	}
-
-	public void registrerBetaling() {
-		// TODO - implement Delfinen.registrerBetaling
-		throw new UnsupportedOperationException();
-	}
-
-	public void hentRestanceOversigt() {
-		// TODO - implement Delfinen.hentRestanceOversigt
-		throw new UnsupportedOperationException();
-	}
-
 	public static void main(String[] args) throws Exception {
 		/*
 		File memberlist=new File("medlemsliste.txt");
@@ -68,40 +51,7 @@ public class Delfinen {
 		Menu.hovedmenu(medlemmer);
 
 
-	/*	ArrayList<Medlem> medlemmer = new ArrayList<>();
-		Scanner sc = new Scanner(System.in);
-		System.out.println("vil du oprette et nyt medlem? ");
-		String userinput = sc.next();
-		if (userinput.equalsIgnoreCase("j")) {
-			do {
-				Medlem.opretMedlem(medlemmer);
-				System.out.println("vil du oprette endnu et medlem? J/N: ");
-				userinput = sc.next();
-			} while (!userinput.equalsIgnoreCase("n"));
 
-		}
-
-		for (Medlem m : medlemmer) {
-			System.out.println(m.printTilKonsol());
-			System.out.println(m);
-			Medlem.skrivMedlemmerTilFil(m);
-		}
-
-		}*/
-		//Konkurrencesvømmer m1=(Konkurrencesvømmer) medlemmer.get(0);
-		//m1.getDiscipliner()[0].opdaterResutater(1);
-		//System.out.println(m1);
-	/*	ArrayList<Medlem> test=new ArrayList<>();
-		try {
-			test= indlæsMedlemmer();
-			System.out.println(test.get(0));
-		}
-		catch (FileNotFoundException f){
-			System.out.println("something went south..sour..sucked.. "+f);
-		}*/
-
-
-		//Indlæs medlemsliste, hvis tom opret nogle medlemmer.
 
 
 
